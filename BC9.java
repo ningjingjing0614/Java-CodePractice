6723f318c6106a60f0f44cc5613855d5c6914c64
ningjngjing0614 <215937309@qq.com>
2021-01-01 21:26:43 +0800

    宁静静：牛客网BC9

---
+++ BC9.java
@@ -0,0 +1,9 @@
+
+public class BC9 {
+    //    包括两行：  第一行为“Hello world!”   第二行为printf(“Hello world!”)调用后的返回值。
+    public static void main(String[] args) {
+        String str = "Hello world!";
+        System.out.println(str);
+        System.out.println(str.length());
+    }
+}
