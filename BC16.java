5483ee56cb812a4d58cee5b846d2a138a3c6a468
ningjngjing0614 <215937309@qq.com>
2021-01-07 21:35:00 +0800

    宁静静：test

---
+++ BC16.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BC16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char ch = str.charAt(0);
+        int ans = ch;
+        System.out.println(ans);
+    }
+}
